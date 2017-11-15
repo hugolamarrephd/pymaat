3ceb97ffb54c5f9c149000f3ac8f2353030a567a
Hugo Lamarre <hugolamarre.phd@gmail.com>
2017-11-15 13:28:58 -0500

    Set up test runner

---
+++ tests/run.py
@@ -0,0 +1,7 @@
+import os
+import unittest
+
+if __name__ == '__main__':
+    TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+    suite = unittest.defaultTestLoader.discover(TEST_DIR)
+    unittest.TextTestRunner().run(suite)
