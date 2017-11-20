92955388c320b9cd15e1c6a4335ddce0ffc46e97
Hugo Lamarre <hugolamarre.phd@gmail.com>
2017-11-20 10:42:43 -0500

    Garch passes all tests + first test for quantizer

--- pymaat/models.py
+++ pymaat/models.py
@@ -7,37 +7,72 @@ class Garch():
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
+        if (1 - self.beta - self.alpha*self.gamma**2 <= 0):
+            raise ValueError
 
-    def filter(self, return_timeseries, first_variance):
-        self.check_variance(first_variance)
-        return_timeseries = np.ascontiguousarray(return_timeseries)
-        s = return_timeseries.shape
-        innovations = np.empty(s, order='C')
-        variance_timeseries = np.empty((s[0]+1,)+s[1:], order='C')
-        variance_timeseries[0] = first_variance
-        for (t,(r,h)) in enumerate(zip(return_timeseries, variance_timeseries)):
-            (variance_timeseries[t+1], innovations[t]) = self.one_step_filter(r,h)
-        return (variance_timeseries, innovations)
+    def filter(self, ret_ts, first_var):
+        self._check_variance(first_var)
 
+        innov_ts = np.empty_like(ret_ts)
+        var_ts = self._init_var_ts(ret_ts, first_var)
 
-    def one_step_filter(self, returns, variances):
-        volatility = np.sqrt(variances)
-        innovations = (returns - (self.mu-0.5) * variances)/volatility
-        next_variances = ( self.omega + self.beta * variances + self.alpha *
-            np.power(innovations - self.gamma * volatility,2) )
-        return (next_variances, innovations)
+        for (t,(r,h)) in enumerate(zip(ret_ts, var_ts)):
+            (var_ts[t+1], innov_ts[t]) = self.one_step_filter(r,h)
 
-    def one_step_simulate(self, innovations, variances):
-        volatility = np.sqrt(variances)
-        returns = (self.mu-0.5) * variances + volatility * innovations
-        next_variances = ( self.omega + self.beta * variances + self.alpha *
-            np.power(innovations-self.gamma * volatility,2) )
-        return (next_variances, returns)
+        return (var_ts, innov_ts)
 
-    def negative_log_likelihood(self, innovations, states):
-        return 0.5 * (np.power(innovations, 2) + np.log(states))
+    def simulate(self, innov_ts, first_var):
+        self._check_variance(first_var)
+
+        ret_ts = np.empty_like(innov_ts)
+        var_ts = self._init_var_ts(innov_ts, first_var)
+
+        for (t,(z,h)) in enumerate(zip(innov_ts, var_ts)):
+            (var_ts[t+1], ret_ts[t]) = self.one_step_simulate(z,h)
+
+        return (var_ts, ret_ts)
+
+    def one_step_filter(self, ret, var):
+        vol = np.sqrt(var)
+        innov = (ret - (self.mu-0.5) * var)/vol
+        next_var = ( self.omega + self.beta * var + self.alpha *
+            np.power(innov - self.gamma * vol,2) )
+        return (next_var, innov)
+
+    def one_step_simulate(self, innov, var):
+        vol = np.sqrt(var)
+        ret = (self.mu-0.5) * var + vol * innov
+        next_var = ( self.omega + self.beta * var + self.alpha *
+            np.power(innov-self.gamma * vol,2) )
+        return (next_var, ret)
+
+    def negative_log_likelihood(self, innov, var):
+        return 0.5 * (np.power(innov, 2) + np.log(var))
 
     @staticmethod
-    def check_variance(variances):
-        if np.any(variances<=0):
+    def _check_variance(var):
+        if np.any(var<=0):
             raise ValueError
+
+    @staticmethod
+    def _init_var_ts(like, first_var):
+        var_shape = (like.shape[0]+1,) + like.shape[1:]
+        var_ts = np.empty(var_shape)
+        var_ts[0] = first_var
+        return var_ts
+
+class Quantizer():
+    def __init__(model, init_innov, first_var):
+        self.model = model
+
+        if init_innov.ndim == 2:
+            self.n_per = init_innov.shape[0]
+            self.n_quant = init_innov.shape[1]
+        else:
+            raise ValueError
+
+        (self.gamma,*_) = self.model.simulate(init_innov, first_var).sort()
+
+    def compute(self):
+        pass
+
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,159 +2,199 @@ import unittest
 
 import numpy as np
 
-import pymaat.models as p
+import pymaat.models
 
+class TestGarchParam(unittest.TestCase):
+    def test_invalid_param_raise_exception(self):
+        with self.assertRaises(ValueError):
+            pymaat.models.Garch(
+                mu=0,
+                omega=0,
+                alpha=0.5,
+                beta=0.5,
+                gamma=1)
 
 class TestGarchFilters(unittest.TestCase):
-    def setUp(self):
-        self.model = p.Garch(
-                mu=2.01,
-                omega=9.75e-20,
-                alpha=4.54e-6,
-                beta=0.79,
-                gamma=196.21
-                )
+    randn = np.array(
+            [[[ 0.31559906, -1.80551443, -0.49468605, -1.04842416],
+            [ 1.47092134, -0.18375408,  0.61439572,  0.64938352]],
+
+           [[-1.62316804, -0.76478879, -0.75826613,  0.17750069],
+            [ 0.82250737,  0.10701289, -1.15691368,  1.05573836]],
+
+           [[-0.22774868,  0.74240245,  1.11521215,  1.10464703],
+            [-0.56968483,  0.54138324, -0.69508153, -0.14254879]]])
+
+    randg = np.array(
+            [[[ 3.40880883,  1.50217308,  1.11125856,  0.14462167],
+            [ 0.08321133,  0.36507093,  1.52133949,  1.42666615]],
+
+           [[ 0.78940018,  0.96257061,  0.3341445 ,  0.59402684],
+            [ 1.67863795,  1.21765875,  1.50332767,  1.03224149]],
+
+           [[ 0.78179542,  1.32126548,  1.64830936,  1.59253612],
+            [ 0.21397881,  0.01596482,  2.48993567,  1.3034988 ]]])
+
+    shape = randg.shape
+
+    model = pymaat.models.Garch(
+            mu=2.01,
+            omega=9.75e-20,
+            alpha=4.54e-6,
+            beta=0.79,
+            gamma=196.21)
+
+    def test_simulate_revertsto_filter(self):
+        innov_ts = self.randn
+        first_var = 1e-7
+        (sim_var_ts, sim_ret_ts) = self.model.simulate(innov_ts, first_var)
+        (fil_var_ts, fil_innov_ts) = self.model.filter(sim_ret_ts, first_var)
+        np.testing.assert_allclose(sim_var_ts, fil_var_ts)
+        np.testing.assert_allclose(innov_ts, fil_innov_ts)
+
+    def test_one_step_simulate_revertsto_filter(self):
+        innov = self.randn
+        var = self.randg
+        sim_next_var, sim_returns = self.model.one_step_simulate(innov,var)
+        fil_next_var, fil_innov = self.model.one_step_filter(sim_returns,var)
+        np.testing.assert_allclose(sim_next_var, fil_next_var)
+        np.testing.assert_allclose(innov, fil_innov)
 
     def test_filter_len2_tuple(self):
-        out = self.model.filter(NORM_RAND,1e-7)
-        self.assertIsInstance(out,tuple)
-        self.assertEqual(len(out),2)
+        out = self.model.filter(self.randn, 1e-7)
+        self.assertIsInstance(out, tuple)
+        self.assertEqual(len(out), 2)
 
     def test_filter_size(self):
-        out = self.model.filter(NORM_RAND,1e-7)
-        s = NORM_RAND.shape
-        self.assertEqual(out[0].shape,(s[0]+1,)+s[1:]) # variance
-        self.assertEqual(out[1].shape,s) # innovations
+        var, innov = self.model.filter(self.randn, 1e-7)
+        expected_var_shape = (self.shape[0]+1,) + self.shape[1:]
+        expected_innov_shape = self.shape
+        self.assertEqual(var.shape, expected_var_shape)
+        self.assertEqual(innov.shape, expected_innov_shape)
 
     def test_filter_positive_variance(self):
-        (h,*_) = self.model.filter(NORM_RAND,1e-7)
-        np.testing.assert_array_equal(h>0,True)
+        (h,*_) = self.model.filter(self.randn, 1e-7)
+        np.testing.assert_array_equal(h>0, True)
 
     def test_filter_throws_exception_when_non_positive_variance(self):
         with self.assertRaises(ValueError):
-            self.model.filter(NORM_RAND,-1)
+            self.model.filter(self.randn, -1)
         with self.assertRaises(ValueError):
-            self.model.filter(NORM_RAND,0)
+            self.model.filter(self.randn, 0)
+
+    def test_simulate_len2_tuple(self):
+        out = self.model.simulate(self.randn, 1e-7)
+        self.assertIsInstance(out, tuple)
+        self.assertEqual(len(out), 2)
+
+    def test_simulate_size(self):
+        var, ret = self.model.simulate(self.randn, 1e-7)
+        expected_var_shape = (self.shape[0]+1,) + self.shape[1:]
+        expected_ret_shape = self.shape
+        self.assertEqual(var.shape, expected_var_shape)
+        self.assertEqual(ret.shape, expected_ret_shape)
+
+    def test_simulate_positive_variance(self):
+        (h,*_) = self.model.simulate(self.randn, 1e-7)
+        np.testing.assert_array_equal(h>0, True)
+
+    def test_simulate_throws_exception_when_non_positive_variance(self):
+        with self.assertRaises(ValueError):
+            self.model.simulate(self.randn, -1)
+        with self.assertRaises(ValueError):
+            self.model.simulate(self.randn, 0)
 
     def test_one_step_filter_len2_tuple(self):
-        out = self.model.one_step_filter(NORM_RAND, GAM_RAND)
-        self.assertIsInstance(out,tuple)
-        self.assertEqual(len(out),2)
+        out = self.model.one_step_filter(self.randn, self.randg)
+        self.assertIsInstance(out, tuple)
+        self.assertEqual(len(out), 2)
 
-    def test_one_step_filter_consistent_size(self):
-        out = self.model.one_step_filter(NORM_RAND, GAM_RAND)
-        self.assertEqual(out[0].size, NORM_RAND.size)
-        self.assertEqual(out[1].size, NORM_RAND.size)
+    def test_one_step_filter_consistent_output(self):
+        out = self.model.one_step_filter(self.randn, self.randg)
+        self.assertEqual(out[0].size, self.randn.size)
+        self.assertEqual(out[1].size, self.randn.size)
+        out = self.model.one_step_filter(self.randn, 1)
+        self.assertEqual(out[0].size, self.randn.size)
+        self.assertEqual(out[1].size, self.randn.size)
+        out = self.model.one_step_filter(0, 1)
+        self.assertIsInstance(out[0], float)
+        self.assertIsInstance(out[1], float)
 
     def test_one_step_filter_positive_variance(self):
-        next_variances, *_ = self.model.one_step_filter(NORM_RAND, GAM_RAND)
-        np.testing.assert_array_equal(next_variances>0,True)
+        next_var, *_ = self.model.one_step_filter(self.randn, self.randg)
+        np.testing.assert_array_equal(next_var>0, True)
+
+    def test_one_step_filter_at_few_values(self):
+        next_var, innov = self.model.one_step_filter(0, 1)
+        self.assertAlmostEqual(innov, 0.5-self.model.mu)
+        self.assertAlmostEqual(next_var,
+                  self.model.omega
+                + self.model.beta
+                + self.model.alpha * (innov - self.model.gamma) ** 2)
 
     def test_one_step_simulate_len2_tuple(self):
-        out = self.model.one_step_simulate(NORM_RAND, GAM_RAND)
+        out = self.model.one_step_simulate(self.randn, self.randg)
         self.assertIsInstance(out,tuple)
         self.assertEqual(len(out),2)
 
-    def test_one_step_simulate_consistent_size(self):
-        out = self.model.one_step_simulate(NORM_RAND, GAM_RAND)
-        self.assertEqual(out[0].size, NORM_RAND.size)
-        self.assertEqual(out[1].size, NORM_RAND.size)
+    def test_one_step_simulate_consistent_output(self):
+        out = self.model.one_step_simulate(self.randn, self.randg)
+        self.assertEqual(out[0].shape, self.shape)
+        self.assertEqual(out[1].shape, self.shape)
+        out = self.model.one_step_simulate(self.randn, 1)
+        self.assertEqual(out[0].shape, self.shape)
+        self.assertEqual(out[1].shape, self.shape)
+        out = self.model.one_step_simulate(0, 1)
+        self.assertIsInstance(out[0], float)
+        self.assertIsInstance(out[1], float)
 
     def test_one_step_simulate_positive_variance(self):
-        next_variances, *_ = self.model.one_step_simulate(NORM_RAND, GAM_RAND)
-        np.testing.assert_array_equal(next_variances>0,True)
+        next_var, *_ = self.model.one_step_simulate(
+                self.randn, self.randg)
+        np.testing.assert_array_equal(next_var>0, True)
 
-    def test_one_step_simulate_revertsto_filter(self):
-        innovations = NORM_RAND
-        current_variances = GAM_RAND
-        sim_nvar, sim_returns = self.model.one_step_simulate(
-            innovations,
-            current_variances
-        )
-        filter_nvar, filter_innov = self.model.one_step_filter(
-            sim_returns,
-            current_variances
-        )
-        np.testing.assert_allclose(sim_nvar,filter_nvar)
-        np.testing.assert_allclose(innovations,filter_innov)
+    def test_one_step_simulate_at_few_values(self):
+        next_var, ret = self.model.one_step_simulate(0, 0)
+        self.assertAlmostEqual(ret, 0)
+        self.assertAlmostEqual(next_var, self.model.omega)
+        next_var, ret = self.model.one_step_simulate(0, 1)
+        self.assertAlmostEqual(ret, self.model.mu-0.5)
+        self.assertAlmostEqual(next_var,
+                  self.model.omega
+                + self.model.beta
+                + self.model.alpha * self.model.gamma ** 2)
+
 
     def test_neg_log_like_at_few_values(self):
-        nll = self.model.negative_log_likelihood(1,1)
-        self.assertAlmostEqual(nll,0.5)
-        nll = self.model.negative_log_likelihood(0,np.exp(1))
-        self.assertAlmostEqual(nll,0.5)
+        nll = self.model.negative_log_likelihood(1, 1)
+        self.assertAlmostEqual(nll, 0.5)
+        nll = self.model.negative_log_likelihood(0, np.exp(1))
+        self.assertAlmostEqual(nll, 0.5)
+
+class TestGarchQuantizer(unittest.TestCase):
+
+    model = pymaat.models.Garch(
+            mu=2.01,
+            omega=9.75e-20,
+            alpha=4.54e-6,
+            beta=0.79,
+            gamma=196.21)
+
+    init_innov = np.array(
+        [[ 0.63466271,  1.31646684,  1.57076979, -1.48338134, -0.67547033,
+        -0.94501199, -1.7518655 ,  1.24787024, -0.24152209, -0.83622565],
+       [-0.0116054 , -0.12164961,  0.35902844,  1.98817555, -0.93355169,
+         0.53536977, -0.93841577, -2.26992583,  1.30517792,  0.1969917 ],
+       [-0.69899308,  0.05944492,  0.53521406, -0.01461253,  2.15017583,
+         0.68809939,  0.17947716, -1.0501256 ,  0.26893459, -0.02800586],
+       [ 0.7633553 ,  1.14672781,  0.06308125,  2.84545437,  0.17366648,
+         0.34276091, -0.48812534, -1.30142566,  1.3140757 ,  1.67758884],
+       [-1.05173776, -0.216082  ,  0.02929778,  0.26620765,  1.88502435,
+        -0.6491563 ,  0.29142118,  0.35421171, -0.49258563,  1.45412617]])
 
 
-
-# Hard-code random sample to avoid test randomness
-NORM_RAND = np.array([  1.24756770e+00,   2.08914936e+00,  -1.27108517e+00,
-    9.24889588e-02,  -2.99486572e+00,   6.63851905e-01,
-    1.99709826e+00,  -1.19870207e+00,  -5.08366828e-02,
-    1.69526956e+00,  -7.98547759e-01,   1.03875675e+00,
-    2.25280331e+00,  -3.88462408e-01,  -3.52430621e-01,
-    2.99320489e-01,   1.39046021e-01,   1.08950341e+00,
-    -8.59475371e-01,  -1.90737253e+00,   5.75872836e-01,
-     1.63256775e+00,   8.73898123e-01,   3.45734054e-01,
-     8.40884289e-01,   1.57190132e-02,   1.28333861e+00,
-    -1.00755543e+00,   4.14665423e-02,   1.28900163e+00,
-    -1.57401313e-01,   1.88485095e-02,  -1.07505006e+00,
-    -9.22017617e-01,   1.66735834e+00,  -1.71799140e-01,
-    -1.40131697e+00,   1.82635715e-01,  -8.57049607e-01,
-     1.77979441e+00,   5.75947019e-01,   5.61500175e-01,
-    -7.27851489e-01,  -1.01416794e+00,  -1.36878081e-01,
-    -1.52275415e-01,  -1.93818774e-01,  -9.00729190e-01,
-     3.68599560e-01,   4.76592952e-01,  -6.91982797e-01,
-    -5.44016364e-01,   1.35430051e-01,  -4.36299354e-01,
-     1.50220293e-01,  -1.63618994e-01,   2.16469417e+00,
-     9.77923098e-01,   1.40195188e+00,   2.21147191e-01,
-    -1.87959787e+00,  -1.09714118e-01,   3.92840047e-01,
-     1.52597899e+00,   3.97661820e-01,   1.58063539e+00,
-    -1.78618050e+00,  -1.58593779e-03,   3.01414171e-01,
-    -1.77420403e-01,   8.77473667e-02,  -1.96126937e+00,
-    -1.62278024e-01,   1.37337028e+00,   2.14470720e+00,
-    -1.95148237e-01,   5.97364250e-01,   2.52435538e-01,
-     6.82272583e-01,   3.34175337e-01,  -9.43967083e-01,
-     1.06867823e+00,   3.35115587e-01,  -2.09461113e-01,
-     2.40275475e+00,  -4.80403315e-01,   3.37966594e-01,
-     9.33910158e-01,   6.31857422e-01,  -2.06248844e-01,
-    -1.32890099e+00,   6.06298897e-01,   7.94792833e-01,
-    -1.02040302e+00,   1.87372330e+00,   3.17392346e-01,
-    -1.49266192e-01,   9.07922623e-01,  -4.30244060e-01,
-    -9.16789507e-01])
-
-GAM_RAND = np.array([  6.63975142e-01,   3.71325713e-01,   2.53582889e-01,
-    2.22130249e+00,   1.51084218e-01,   5.09197754e-01,
-    2.03723994e+00,   1.79911538e+00,   1.25810450e-01,
-    3.61231684e-01,   1.48801360e-01,   5.79185431e-01,
-    5.70774882e-02,   1.70161117e-01,   1.94311311e-01,
-    2.82084295e-01,   3.78866053e+00,   6.42739247e-01,
-    5.65659499e-02,   2.41782632e-01,   8.61487180e-01,
-    3.99470968e-01,   4.15261744e-01,   8.84963260e-04,
-    1.91341159e+00,   1.05887825e+00,   8.62162874e-01,
-    3.15780374e-03,   3.58233691e-01,   4.43784333e-02,
-    5.04928598e+00,   1.49859295e-01,   8.81756599e-01,
-    1.38236593e+00,   9.36651240e-01,   1.15030619e-01,
-    6.29524158e-02,   7.77507463e-01,   2.53345618e-01,
-    7.10992681e-01,   4.24851815e-01,   2.78275428e-01,
-    3.02120168e-01,   2.51910420e+00,   9.14999770e-01,
-    3.13114032e+00,   5.84429189e+00,   8.64073072e-01,
-    1.31352514e-01,   1.67567325e-01,   1.45310823e-01,
-    7.24637864e-01,   6.32539520e-01,   2.88729565e-01,
-    1.08753036e-01,   5.29744517e-01,   8.58559262e-01,
-    9.40136092e-01,   9.05376519e-01,   1.13208009e-01,
-    4.63924587e+00,   2.45102470e-01,   1.04570973e+00,
-    4.53022904e+00,   9.71037042e-01,   3.38598305e+00,
-    1.44280305e+00,   3.14878709e-01,   6.10256720e-01,
-    9.38642200e-01,   2.41285232e+00,   2.10886942e-02,
-    9.54472940e-01,   1.43308762e+00,   3.54300368e-02,
-    3.39800422e+00,   1.31227604e+00,   6.19759788e-02,
-    1.26040143e-01,   2.33726475e+00,   5.92150516e-01,
-    8.99138356e-02,   3.72322230e-01,   2.39297476e-01,
-    3.65711971e-03,   5.71818303e-01,   8.66844065e-01,
-    6.15089711e-01,   6.74700311e-01,   1.10897769e+00,
-    1.78757703e-01,   1.13328851e+00,   7.46080404e-01,
-    1.33518328e+00,   1.27194919e-02,   1.91418688e+00,
-    3.46539716e+00,   3.49583230e+00,   1.16530687e+00,
-    9.88330735e-01])
-
-
+    def setUp(self):
+        self.quant = pymaat.models.Quantizer(
+                self.model,
+                self.init_innov,
+                1e-7)
